694a395b535700b8f82817208e5e043d4dd92c5e
xyt-dev <417639959@qq.com>
2026-05-16 22:57:08 +0800

    add rspress-katex

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,5 +1,6 @@
 import * as path from 'node:path';
 import { defineConfig } from '@rspress/core';
+import katex from 'rspress-plugin-katex';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
@@ -13,4 +14,15 @@ export default defineConfig({
     socialLinks: [
     ],
   },
+  markdown: {
+    shiki: {
+      onError(error) {
+        if (error instanceof Error && error.message.includes('Language `math`')) {
+          return;
+        }
+        throw error;
+      },
+    },
+  },
+  plugins: [katex()],
 });
