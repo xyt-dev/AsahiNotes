aef81ab1323aa7ea5f6995e971c3d6221e07afa7
xyt-dev <417639959@qq.com>
2026-05-16 19:11:12 +0800

    use rspress

---
+++ rspress.config.ts
@@ -0,0 +1,16 @@
+import * as path from 'node:path';
+import { defineConfig } from '@rspress/core';
+
+export default defineConfig({
+  root: path.join(__dirname, 'docs'),
+  title: 'AsahiNotes',
+  icon: '/rspress-icon.png',
+  // logo: {
+  //   light: '/rspress-light-logo.png',
+  //   dark: '/rspress-dark-logo.png',
+  // },
+  themeConfig: {
+    socialLinks: [
+    ],
+  },
+});
